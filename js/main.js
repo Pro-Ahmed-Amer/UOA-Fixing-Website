a93aaa7c663a694152d9f029288a470ec3cca7aa
unknown <ahmedprogrammer387@gmail.com>
2020-08-17 00:53:44 +0300

    My Fixing UOA-Website

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('#sidebarCollapse').on('click', function() {
+        $('#sidebar').toggleClass('active');
+    });
+});
+$(document).ready(function() {
+    $('#ar-sidebarCollapse').on('click', function() {
+        $('#ar-sidebar').toggleClass('active');
+    });
+});
+/*Start By Pro-Ahmed Amer*/
+$(document).ready(function() {
+    $('#sidebarCollapse').on('click', function() {
+        $('#main-page').toggleClass('sidebar-fix');
+    });
+});
+$(document).ready(function() {
+    $('#ar-sidebarCollapse').on('click', function() {
+        $('#main-page').toggleClass('sidebar-fix');
+    });
+});
+/*End By Pro-Ahmed Amer*/
